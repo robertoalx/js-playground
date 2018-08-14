ac53f079ea7f16a92c1b9989e863699a4ccf0f53
robertoalx <romaniarobertverde@yahoo.com>
2018-08-14 10:10:03 +0100

    fix minor issue for linter.

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import other from './other';
 import {getMeUnique, sumNumbers} from './es6-unique-array';
 // import {raceNumber, early, age} from './time-to-race';
 // import playback from './rock-paper-scissors';
-import {storyWords, sentencesCount, reallyCount, veryCount, basicallyCount}from './linter';
+import {storyWords, sentenceCount, reallyCount, veryCount, basicallyCount, betterWords}from './linter';
 
 // other('Others,');
 // console.log(getMeUnique([1, 2, 3, 2, 4, 2, 1, 6]))
 // console.log(sumNumbers(1, 2, 3, 2, 4, 2, 1, 6))
 // console.log(age, raceNumber);
 // playback
-console.log("Word count: " + storyWords.length);
-console.log("Sentence count " + sentencesCount);
-console.log("really: " + reallyCount);
-console.log("very: " + veryCount);
-console.log("basically: " + basicallyCount);   
+console.log("Sentence count ", sentenceCount);
+console.log("really: ", reallyCount);
+console.log("very: ", veryCount);
+console.log("basically: ", basicallyCount);   
